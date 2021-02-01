6322ef290b6004cc2ee43b048a2edfae7e270e4a
oguzhanoxel <oguzhanoksel@hotmail.com>
2021-02-01 16:33:03 +0300

    initial commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,60 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            List<Car> _cars = carManager.GetAll();
+
+            void MyList(string listTitle) {
+                Console.WriteLine("\n\n" + listTitle.ToUpper());
+                foreach (var item in carManager.GetAll())
+                {
+                    Console.WriteLine(
+                        "\n" +
+                        "id = " + item.Id + "\n" +
+                        "brand id = " + item.BrandId + "\n" +
+                        "color id = " + item.ColorId + "\n" +
+                        "model year = " + item.ModelYear.Year + "\n" +
+                        "daily price = " + item.DailyPrice + "\n" +
+                        "desc = " + item.Description
+                    );
+                }
+            }
+
+            MyList("First List"); // İlk liste
+
+            Car newCar = new Car();
+            newCar.Id = 6;
+            newCar.BrandId = 1;
+            newCar.ColorId = 2;
+            newCar.ModelYear = new DateTime(2013);
+            newCar.DailyPrice = 700;
+            newCar.Description = "Description5";
+            
+            carManager.Add(newCar);
+
+            MyList("Added car, Car ID = " + newCar.Id); // yeni araba eklendikden sonra
+
+            Car carToDelete = _cars.SingleOrDefault(p => p.Id == 3); // kullanıcıdan id si 3 olan arabayı geldiği varsayılı
+            carManager.Delete(carToDelete);
+
+            MyList("Deleted car, Car ID = " + carToDelete.Id); // id si 3 olan arabanin silindiği liste
+
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == 2); // kullanıcı veya adminin id si 2 olan arabayi güncellediği varsayılı
+            carToUpdate.Description = "Updated Description";
+            carToUpdate.DailyPrice = 799;
+
+            MyList("Updated Car, Car ID = " + carToUpdate.Id); // id si 2 olan arabanin güncellendiği liste
+
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,53 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> { 
+                new Car{Id = 1, BrandId = 1, ColorId = 1, ModelYear = new DateTime(2010), DailyPrice = 1000, Description = "Description"},
+                new Car{Id = 2, BrandId = 1, ColorId = 1, ModelYear = new DateTime(2016), DailyPrice = 999, Description = "Description1"},
+                new Car{Id = 3, BrandId = 2, ColorId = 1, ModelYear = new DateTime(2011), DailyPrice = 500, Description = "Description2"},
+                new Car{Id = 4, BrandId = 2, ColorId = 2, ModelYear = new DateTime(2017), DailyPrice = 2000, Description = "Description3"},
+                new Car{Id = 5, BrandId = 2, ColorId = 2, ModelYear = new DateTime(2012), DailyPrice = 1900, Description = "Description4"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(p => p.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+        public Car GetById(int car_id)
+        {
+            Car car = _cars.SingleOrDefault(p => p.Id == car_id);
+            return car;
+        }
+    }
+}
