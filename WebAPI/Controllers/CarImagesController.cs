1a49e76fdc3e30a8b23e314448fd125a9978b98e
oguzhanoxel <oguzhanoksel@hotmail.com>
2021-02-26 17:22:08 +0300

    UploadFile

--- WebAPI/Controllers/CarImagesController.cs
+++ WebAPI/Controllers/CarImagesController.cs
@@ -1,10 +1,13 @@
 ﻿using Business.Abstract;
 using Entities.Concrete;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -13,10 +16,12 @@ namespace WebAPI.Controllers
     public class CarImagesController : ControllerBase
     {
         ICarImageService _carImageService;
+        private static IWebHostEnvironment _webHostEnvironment;
 
-        public CarImagesController(ICarImageService carImageService)
+        public CarImagesController(ICarImageService carImageService, IWebHostEnvironment webHostEnvironment)
         {
             _carImageService = carImageService;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         [HttpGet("getall")]
@@ -29,6 +34,7 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
         [HttpGet("getbyid")]
         public IActionResult GetById(int id)
         {
@@ -39,6 +45,7 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
         [HttpGet("getimagesbycarid")]
         public IActionResult GetImagesByCarId(int id)
         {
@@ -49,6 +56,7 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
         [HttpGet("details")]
         public IActionResult GetCarDetails()
         {
@@ -59,9 +67,42 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
         [HttpPost("add")]
-        public IActionResult Add(CarImage carImage)
+        public IActionResult Add([FromForm] CarImageModel carImageModel)
         {
+            CarImage carImage = new CarImage();
+
+            try
+            {
+                if(carImageModel.ImageFile.Length > 0)
+                {
+                    string path = _webHostEnvironment.WebRootPath + "\\images\\";
+                    if (!Directory.Exists(path))
+                    {
+                        Directory.CreateDirectory(path);
+                    }
+                    string filePath = path + carImageModel.FileName + ".jpg";
+                    carImage.ImagePath = filePath;
+                    using (FileStream fileStream = System.IO.File.Create(filePath))
+                    {
+                        carImageModel.ImageFile.CopyTo(fileStream);
+                        fileStream.Flush();
+                    }
+                }
+                else
+                {
+                    return BadRequest("Not Uploaded.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+
+            carImage.CarID = carImageModel.CarID;
+            carImage.Date = DateTime.Now;
             var result = _carImageService.Add(carImage);
             if (result.Success)
             {
@@ -69,6 +110,7 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
         [HttpPost("delete")]
         public IActionResult Delete(CarImage carImage)
         {
@@ -79,6 +121,7 @@ namespace WebAPI.Controllers
             }
             return BadRequest(result);
         }
+
         [HttpPost("update")]
         public IActionResult Update(CarImage carImage)
         {
---
+++ WebAPI/Models/CarImageModel.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class CarImageModel:CarImage
+    {
+        public IFormFile ImageFile { get; set; }
+        public string FileName { get; set; }
+    }
+}
